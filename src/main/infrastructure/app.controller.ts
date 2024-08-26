81ad494fbe57616f7c869ee3d11770e99bbb5f15
Pedro Macías <pedro@tecnual.com>
2024-08-26 17:22:08 +0200

    style: :rotating_light: linter configurations

--- src/main/infrastructure/app.controller.ts
+++ src/main/infrastructure/app.controller.ts
@@ -1,16 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../application/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): any {
-    const data = {
-      name: 'Pedro',
-      location: 'Fabero',
-    };
-    return data;
+  getHello() {
+    return 'Ok';
   }
 }
